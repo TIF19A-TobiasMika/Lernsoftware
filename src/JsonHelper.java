bfb78a4916009e0eac06821e7f4a8935f08b9ccf
TW3005 <60391348+TW3005@users.noreply.github.com>
2020-01-28 16:24:59 +0100

    Added ReturnCategoriesAndQuestions Method.

--- src/JsonHelper.java
+++ src/JsonHelper.java
@@ -1,10 +1,16 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class JsonHelper {
 
 
 
     static ArrayList<Question> getQuestionsFromFile(String path) {
+        return null;
+    }
+
+    static HashMap<String, ArrayList<Question>> ReturnQuestionsandCategories()
+    {
         return null;
     }
 
