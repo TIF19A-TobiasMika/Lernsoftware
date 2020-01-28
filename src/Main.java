e2a1da757d1f5a6f8c5d8700b1f8046f623ced9a
Mika Busch <haeki@hotmail.de>
2020-01-28 14:55:49 +0100

    Added Base Classes

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Question q1 = new Question("Frage 1", "Antwort 1");
+
+        
+    }
+}
---
+++ src/Question.java
@@ -0,0 +1,9 @@
+public class Question {
+    String question;
+    String answer;
+
+    public Question(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
