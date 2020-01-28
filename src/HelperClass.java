549cffac7d26b92edef3df5ce9314df8e3d00c5c
Mika Busch <haeki@hotmail.de>
2020-01-28 15:13:18 +0100

    updated .gitignore

--- src/HelperClass.java
+++ src/HelperClass.java
@@ -1,4 +1,16 @@
 public class HelperClass {
-    
+
+    static String createChoiceMenuString(String title, String[] options) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(title);
+        sb.append("\n");
+        for (int i = 0; i < options.length; i++) {
+            sb.append(i+1);
+            sb.append(") ");
+            sb.append(options[i]);
+            sb.append('\n');
+        }
+        return  sb.toString();
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Question q1 = new Question("Frage 1", "Antwort 1");
+        System.out.println(HelperClass.createChoiceMenuString("Test", new String[] {"Option A", "Option B", "Option C"}));
 
-        
     }
 }
