703a62043423ac4f976eef795a487568ab0858cb
TW3005 <60391348+TW3005@users.noreply.github.com>
2020-01-28 16:07:55 +0100

    Added Helper Methods

--- src/HelperClass.java
+++ src/HelperClass.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class HelperClass {
+
+    static Scanner scanner = new Scanner(System.in);
 
     static String createChoiceMenuString(String title, String[] options) {
         StringBuilder sb = new StringBuilder();
@@ -11,6 +15,57 @@ public class HelperClass {
             sb.append('\n');
         }
         return  sb.toString();
+    }
+
+    static String GetInputText(String question)
+    {
+        System.out.print(question);
+        return scanner.nextLine();
+    }
+
+    static int GetInputInt(String question)
+    {
+        System.out.print(question);
+
+        while(true)
+        {
+            var input = scanner.nextLine();
+
+            try
+            {
+                var intInput = Integer.parseInt(input);
+                return intInput;
+            }
+            catch (Exception e)
+            {
+                System.out.print("Invalid input. Please write a number.");
+                continue;
+            }
+        }
+    }
+
+    static int GetInputInt(String question, int startBoundary, int endBoundary)
+    {
+        System.out.print(question);
+
+        while(true)
+        {
+            var input = scanner.nextLine();
+
+            try
+            {
+                var intInput = Integer.parseInt(input);
+
+                if(startBoundary> intInput || endBoundary < intInput) continue;
+
+                return intInput;
+            }
+            catch (Exception e)
+            {
+                System.out.print("Invalid input. Please write a number.");
+                continue;
+            }
+        }
     }
 
 }
