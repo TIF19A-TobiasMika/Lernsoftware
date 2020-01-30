73e8384efa296463838bc15fe428082ec2c9d837
Mika Busch <haeki@hotmail.de>
2020-01-30 13:08:16 +0100

    First gson implementation

--- src/JsonHelper.java
+++ src/JsonHelper.java
@@ -1,17 +1,49 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class JsonHelper {
 
 
-
     static ArrayList<Question> getQuestionsFromFile(String path) {
+        try (JsonReader jsonReader = new JsonReader(new FileReader(path))) {
+            Gson gson = new Gson();
+            return gson.fromJson(jsonReader, new TypeToken<ArrayList<Question>>() {}.getType());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static HashMap<String, ArrayList<Question>> ReturnQuestionsAndCategories()
+    {
         return null;
     }
 
-    static HashMap<String, ArrayList<Question>> ReturnQuestionsandCategories()
-    {
-        return null;
+    static void saveQuestionsToFile(ArrayList<Question> questions, String path) {
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setPrettyPrinting();
+            Gson gson = gsonBuilder.create();
+            String json = gson.toJson(questions);
+            System.out.println(json);
+            //Write JSON file
+            try (FileWriter file = new FileWriter(path)) {
+
+                file.write(json);
+                file.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
     }
 
 }
--- src/Question.java
+++ src/Question.java
@@ -1,9 +1,25 @@
 public class Question {
     String question;
     String answer;
+    String[] alternateAnswers;
 
-    public Question(String question, String answer) {
+    public Question(String question, String answer, String[] alternateAnswers) {
         this.question = question;
         this.answer = answer;
+        this.alternateAnswers = alternateAnswers;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder tmp = new StringBuilder("Question: ");
+        tmp.append(question);
+        tmp.append("\nAnswer: ");
+        tmp.append(answer);
+        tmp.append("\nAlternate Answers: ");
+        for (String str : alternateAnswers) {
+            tmp.append("\n  ");
+            tmp.append(str);
+        }
+        return tmp.toString();
     }
 }
